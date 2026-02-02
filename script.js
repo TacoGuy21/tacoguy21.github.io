2342463a41c62736ce670a0388460d07915a3bcb
Hubert <119009502+TacoGuy21@users.noreply.github.com>
2026-02-02 16:28:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
 const API_KEY = "AIzaSyD0p898HsiRxxIMHiaZYVhzFVl_Ux0SNKI";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
   message: null,
